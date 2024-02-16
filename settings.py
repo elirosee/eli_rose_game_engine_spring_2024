3290fa622c159ea21e6ff59b0ee3441cde73da62
elirosee <160270244+elirosee@users.noreply.github.com>
2024-02-16 10:18:01 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+# This file was created by: Eli Rose
+TITLE = "my fun game"
+
+FPS = 30
+
+PLAYER_SPEED = 300
+
+# this is 32 grids wide 
+WIDTH = 1024
+# this is 24 grids high
+HEIGHT = 768
+TILESIZE = 32
+
+SKYBLUE = (111, 186, 234)
+
+LIGHTGREY = (100,100,100)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255,0,0)
+BGCOLOR = SKYBLUE
---
+++ sprites.py
@@ -0,0 +1,101 @@
+# This file was created by: eli rose
+
+# import modules
+import pygame as pg
+from pygame.sprite import Sprite
+from settings import *
+
+# create a player class
+
+class Player(Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.vx, self.vy = 0, 0
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+
+    # def move(self, dx=0, dy=0):
+    #     self.x += dx
+    #     self.y += dy
+        
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.top - self.rect.height
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+
+
+    def update(self):
+        # self.rect.x = self.x * TILESIZE
+        # self.rect.y = self.y * TILESIZE
+        self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        # add x collision later
+        self.collide_with_walls('x')
+        # add y collision later
+        self.rect.y = self.y
+        
+
+class Wall(Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+class PowerUp(Sprite):
+    def __init__(self, game, x, y):
+        # add powerup groups later....
+        self.groups = game.all_sprites
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+
